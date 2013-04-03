e3aa1f459187e32f40982c0fbf5a3376566dfdb9
Pasha <Pasha@MICROSOFT-PC.Dlink>
2013-04-03 11:11:45 +0300

    add new fields

--- application/modules/admin/forms/LoginForm.php
+++ application/modules/admin/forms/LoginForm.php
@@ -5,7 +5,7 @@ class modules_admin_forms_LoginForm extends forms_FormBase
 	{
 		$oEmailElement = new Zend_Form_Element_Text('email');
 		$oEmailElement = $this->_initDefaultFiltersAndValidators($oEmailElement);
-		$oEmailElement->addValidator(new Zend_Validate_EmailAddress());
+//		$oEmailElement->addValidator(new Zend_Validate_EmailAddress());
 		$this->addElement($oEmailElement);
 
 		$oPasswordElement = new Zend_Form_Element_Password('password');
